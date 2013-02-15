1adaced26d479ac5cea8e688c88352d52cc51bc4
Sam-Ayyagari <sayyagari@innomindscom>
2013-02-15 13:50:19 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+//To check git changes
+
+var innoCricket = angular.module('innoCricket', []);
+
+//Routing starts here
+  innoCricket.config(function($routeProvider){
+    $routeProvider
+    .when('/',{templateUrl:'views/update.html', controller:'HomeController'})
+    .when ('/profiles',{templateUrl:'views/profiles.html', controller:'ProfilesController'})
+    .when ('/gallery',{templateUrl:'views/update.html', controller:'ProfilesController'})
+    .when ('/previous',{templateUrl:'views/update.html', controller:'ProfilesController'})
+    .when('/player/:playerId', {templateUrl:'views/player-Info.html', controller:'PlayerDetailsController'})
+
+  });
